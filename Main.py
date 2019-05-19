9182e37b865d030f79781e169c7da3c0e865a4a3
Jon Dotsoy <hi@jon.soy>
2019-05-18 22:20:26 -0400

    main

--- Main.py
+++ Main.py
@@ -1,4 +1,15 @@
 import visual
+import match
+import Mein
+
+
+def main():
+    P = match.create_random_points(30)
+    R = Mein.heuristica(P)
+    return visual.Window(rectangles=R, points=P)
+
 
 if __name__ == '__main__':
-    visual.mainloop()
+    app = main()
+    app.root.mainloop()
+    app.paper.scale()
